214abcaa906fdde817cd3382c73607d24e9d23f6
Ashik150 <sskhan4409@gmail.com>
2025-05-19 22:40:10 +0600

    Image Basic

--- practise/app/about/mission/page.js
+++ practise/app/about/mission/page.js
@@ -1,9 +1,12 @@
 import Button from '@/app/components/Button'
+import Image from 'next/image'
+import thumb from "@/public/images/thumb.jpeg"   
 
 export default function Mission() {
     return (
         <main className="mt-10">
             <div>This is mission page in about</div>
+            <Image src = {thumb} alt = "Thumb Image" />
             <Button/>
         </main>
     )
